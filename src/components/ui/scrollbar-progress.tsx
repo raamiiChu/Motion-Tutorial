5bb9bbea2d6dd13209e0ab9eb4c3977b105fb497
Ramy Chu <ericchu0617@gmail.com>
2025-05-22 17:37:43 +0800

    feat: scrollbar progress

---
+++ src/components/ui/scrollbar-progress.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { motion, useScroll } from "motion/react";
+
+const ScrollbarProgress = () => {
+  const { scrollYProgress } = useScroll();
+
+  return (
+    <motion.div
+      id="scroll-indicator"
+      className="fixed inset-x-0 bottom-0 z-20 h-2.5 origin-left bg-blue-400"
+      style={{
+        scaleX: scrollYProgress,
+      }}
+    />
+  );
+};
+
+export default ScrollbarProgress;
