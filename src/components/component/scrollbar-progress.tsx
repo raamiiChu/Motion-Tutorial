ae957ab90f21a579bb95c23528746fa304dc27df
raamiiChu <ericchu0617@gmail.com>
2025-06-29 21:54:39 +0800

    style: add role

--- src/components/component/scrollbar-progress.tsx
+++ src/components/component/scrollbar-progress.tsx
@@ -7,7 +7,7 @@ const ScrollbarProgress = () => {
 
   return (
     <motion.div
-      id="scroll-indicator"
+      role="progressbar"
       className="fixed inset-x-0 bottom-0 z-20 h-1.5 origin-left bg-blue-400"
       style={{
         scaleX: scrollYProgress,
