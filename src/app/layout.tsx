5ae9773a87b4e7a0b722c43a845026c7924bd207
raamiiChu <ericchu0617@gmail.com>
2025-06-29 16:55:45 +0800

    style: padding with rwd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             <main className="relative w-full">
               <ScrollbarProgress />
               <SidebarTrigger className="bg-background border-accent sticky top-0 z-50 border-2 md:border-0" />
-              <article className="container mx-auto px-4 py-12 md:px-24 xl:px-40">
+              <article className="container mx-auto px-4 py-12 md:px-12 lg:px-24 xl:px-40 2xl:px-64">
                 {children}
               </article>
             </main>
